be1dcab0f3a4c7c5cbe450db58e570de24dde839
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-10-21 21:02:09 +0300

    Hooks: useState()

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from '../components/Header'
 import List from '../components/List'
 import Description from '../components/Description'
 import Link from 'next/link'
+import { useState } from 'react'
 
 
 function Home() {
-  let showDescription=true;
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () =>  {
+  setCounter(counter+1)
+  }
 
   return (
     <main className={styles['main-hi']}>
-      <p>Hello React!</p>
-      <Header text='Header component' textSub='hi' />
-      {showDescription && <Description />}
-      <List description="This is a description"/>
-      <Link href="/about">Go to About</Link>
+      <h1>Home</h1>
+      <div>Counter: {counter}</div>
+      <button onClick={handleClick}>Click to increment</button>
     </main>
   )
 }
